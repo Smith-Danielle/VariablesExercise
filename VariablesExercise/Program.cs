27bcfb2ab14afc0dbbeacbcd938ee053cafd6022
Danielle Smith <smith.ddms@gmail.com>
2021-06-10 10:48:36 -0500

    Completed Variables Exercise

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Kim";
+            int age = 22;
+            char firstInitial = 'K';
+            bool isEmployed = true;
+            double averageDistance = 2.3;
+            decimal hoursWorked = 10.55m;
+
+
+
+            Console.WriteLine($"My name is {name} and I am {age} years old.");
+            Console.WriteLine($"Most people just call me {firstInitial} for short.");
+            Console.WriteLine($"It is {isEmployed} that I am employed by Verison.");
+            Console.WriteLine($"My job is about {averageDistance} miles from my home and I work about {hoursWorked} hours a week."); 
+
+        }
+    }
+}
